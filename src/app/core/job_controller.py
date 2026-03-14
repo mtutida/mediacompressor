97d0b066314912f37d520e290b3301c392f0c252
mtutida <marcelo.tutida@gmail.com>
2026-03-14 20:46:04 -0300

    fix: job controller integration

--- src/app/core/job_controller.py
+++ src/app/core/job_controller.py
@@ -1,8 +1,15 @@
 class JobController:
 
-    def __init__(self, model):
+    def __init__(self, model, scheduler):
         self.model = model
+        self.scheduler = scheduler
 
     def enqueue(self, job):
+
+        # adiciona na UI
         if hasattr(self.model, "add_job"):
             self.model.add_job(job)
+
+        # envia para execução
+        if self.scheduler:
+            self.scheduler.enqueue(job)
